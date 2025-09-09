f2f7c79435f5d21ddb5ce5eae06ca98f596a8578
carolyne-wanjiru <shirogithenduka@gmail.com>
2025-09-09 17:40:20 +0300

    made changes to the index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ const io = new Server(server, {
 });
    
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173",],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
